25899aa4b801dad75728e0be4d4be5605b635401
Sparsh Agarwal <agarwal39@wisc.edu>
2017-04-04 00:16:15 -0500

    comments added

--- app/components/game-list.js
+++ app/components/game-list.js
@@ -3,16 +3,16 @@ import Ember from 'ember';
 var data;
 
 export default Ember.Component.extend({
-	actions: {
-		up(url,page) {
+	actions: {						// the two actions are for navigation arrows
+		up(url,page) {				// the up action is for moving forward, it updates the stream list with next list. 
 			var self = this;
 			var requrl = url.concat('&client_id=t171hp309vgcm3wxbqp801hscpjni4');
 			Ember.$.getJSON(requrl, function(result) {
-				Ember.set(result,'page', page+1);
+				Ember.set(result,'page', page+1);			//page+1 updates the page number between the arrows.
 				self.set('model', result);
 			});
 		},
-		down(url,page) {
+		down(url,page) {			// the down action is for moving backwards, it updates the stream list with previous list. 
 			var self = this;
 			var requrl = url.concat('&client_id=t171hp309vgcm3wxbqp801hscpjni4');
 			Ember.$.getJSON(requrl, function(result) {
--- app/routes/home.js
+++ app/routes/home.js
@@ -7,13 +7,13 @@ export default Ember.Route.extend({
 		// return model;
 		return Ember.$.getJSON('https://api.twitch.tv/kraken/search/streams?limit=5&client_id=t171hp309vgcm3wxbqp801hscpjni4&query=starcraft');
 	},   
-	actions: {
+	actions: {		// the search action is for using the search parameter and compute the url to finally get the json data.
 		search: function(parameter) {
 			var self=this;
 			requrl = ('https://api.twitch.tv/kraken/search/streams?limit=5&client_id=t171hp309vgcm3wxbqp801hscpjni4&query=').concat(parameter);
 			Ember.$.getJSON(requrl, function(result) {
-				Ember.set(result,'page', 1);
-				Ember.set(result,'_totalpages',Math.ceil(result._total/5));
+				Ember.set(result,'page', 1);		//setup the base page number
+				Ember.set(result,'_totalpages',Math.ceil(result._total/5));		//setup the total number of pages
 				self.set('model', result);
 			});			
 		}
